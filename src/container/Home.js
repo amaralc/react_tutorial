a19a924565690a7976174217de2461fd3f8f507e
amaralc <32170099+amaralc@users.noreply.github.com>
2019-07-18 21:49:08 -0300

    Deleted three HomeCard components
    Sync test between codesandbox and git

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
-import HomeCard from '../ui/HomeCard';
+import React, { Component } from "react"
+import HomeCard from "../ui/HomeCard"
 
 class Home extends Component {
+  homeCardReleases = {
+    title: "Releases",
+    text: " Manage Releases",
+    action: () => alert("Releases Clicked")
+  }
 
-    homeCardReleases = {
-        title: 'Releases',
-        text: ' Manage Releases',
-        action: () => alert('Releases Clicked')
-    }
+  homeCardBacklog = {
+    title: "Backlog",
+    text: " Manage Backlog",
+    action: () => alert("Backlog Clicked")
+  }
 
-    homeCardBacklog = {
-        title: 'Backlog',
-        text: ' Manage Backlog',
-        action: () => alert('Backlog Clicked')
-    }
+  homeCardSprints = {
+    title: "Sprints",
+    text: " Manage Sprints",
+    action: () => alert("Sprints Clicked")
+  }
 
-    homeCardSprints = {
-        title: 'Sprints',
-        text: ' Manage Sprints',
-        action: () => alert('Sprints Clicked')
-    }
-
-    render() {
-        return (
-            <div className="container text-center">
-                <div className="row">
-                    <HomeCard
-                        title={this.homeCardReleases.title}
-                        text={this.homeCardReleases.text}
-                        action={this.homeCardReleases.action}
-                    />
-                    <HomeCard {...this.homeCardBacklog} />
-                    <HomeCard {...this.homeCardSprints} />
-                    <HomeCard {...this.homeCardSprints} />
-                    <HomeCard {...this.homeCardSprints} />
-                    <HomeCard {...this.homeCardSprints} />
-                </div>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container text-center">
+        <div className="row">
+          <HomeCard
+            title={this.homeCardReleases.title}
+            text={this.homeCardReleases.text}
+            action={this.homeCardReleases.action}
+          />
+          <HomeCard {...this.homeCardBacklog} />
+          <HomeCard {...this.homeCardSprints} />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Home;
+export default Home
